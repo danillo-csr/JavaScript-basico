b78c085f1fcf79c90fe36eb855bc00280ef21a1d
danillo cesar <danillocesar_06@hotmail.com>
2020-07-04 17:46:17 -0300

    correção

--- 04-js-assíncrono/desafios-03/55main-02.js
+++ 04-js-assíncrono/desafios-03/55main-02.js
@@ -6,26 +6,24 @@ function busca(){
 var inElement = document.querySelector('#in');
 var user = inElement.value;
 
-axios.get('https://api.github.com/users/'+user+"/repos")
+
+axios.get('https://api.github.com/users/'+user+'/repos')
 .then(function(response){
     //console.log(response);
-    if(response.data.length != 0){
+    if(response.data != null){
         response.data.forEach(repo => {
             var liElement = document.createElement('li');
             var text = document.createTextNode(repo.name);
             liElement.appendChild(text);
-            //console.log(repo.name);
             var ulElement = document.querySelector('#repositorios');
             ulElement.appendChild(liElement);
-        })
-    } 
-    else{
-        alert("O usuário informado não possui repositórios públicos!");  
+        });
+    } else {
+        alert("O perfil foi encontrado mas não possui repositórios públicos!");
     }
 })
 .catch(function(error){
     alert("Perfil de usuário não encontrado, tente novamente!");
     //console.warn(error);
 });
-
 }
